756c7588187e5d4761322b60b91eb2f5d2eb2940
MarcoSavarin0 <ateneo2003@gmail.com>
2024-07-13 18:54:17 -0300

    estilo del home hecho, falta footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,4 +6,31 @@ window.addEventListener("load", function () {
         hamburguer.classList.toggle("fa-bars");
         hamburguer.classList.toggle("fa-xmark");
     });
+    const siBox = document.querySelector('.main-container__content__section-question__box-answer_si');
+    const noBox = document.querySelector('.main-container__content__section-question__box-answer_no');
+
+    siBox.addEventListener('click', function () {
+        siBox.classList.add('active');
+        noBox.classList.remove('active');
+    });
+
+    noBox.addEventListener('click', function () {
+        noBox.classList.add('active');
+        siBox.classList.remove('active');
+    });
+    document.getElementById('toggle-icon').addEventListener('click', function() {
+        var referencias = document.getElementById('referencias');
+        referencias.classList.toggle('open');
+        
+        var icon = this.querySelector('i');
+        if (referencias.classList.contains('open')) {
+            icon.classList.remove('fa-plus');
+            icon.classList.add('fa-minus');
+        } else {
+            icon.classList.remove('fa-minus');
+            icon.classList.add('fa-plus');
+        }
+    });
+    
+
 });
